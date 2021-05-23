bad05a7ecaabb54034d5b64a52a9972d9cc85ba7
Adam <mail2gzs@gmail.com>
2021-05-23 23:24:14 +0200

    allMovies, findById, addMovie, updateMovie, deleteMovie

--- src/main/java/adaguz/movieservice/service/MovieRepository.java
+++ src/main/java/adaguz/movieservice/service/MovieRepository.java
@@ -3,6 +3,20 @@ package adaguz.movieservice.service;
 import adaguz.movieservice.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Override
+    <S extends Movie> S save(S entity);
+
+    @Override
+    List<Movie> findAll();
+
+    @Override
+    Optional<Movie> findById(Long aLong);
+
+    @Override
+    void delete(Movie entity);
 }
